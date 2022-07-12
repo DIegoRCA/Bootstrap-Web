21e29738bbd45f38885b429a7a93a491cf37a9a5
DiegoRCA <diegocartelle@gmail.com>
2022-07-12 16:51:26 +1000

    contact

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,44 @@
 import React from 'react';
-function Contact() {
-return <div>THIS IS THE CONTACT PAGE</div>;
+import '../styles/pages/ContactPage.css';
+
+const ContactPage = (props)=>{
+    return(
+        <main className="holder wrapper">
+            <div className="columns">
+                <div className="leftColumn">
+                    <ul>
+                        <li>13-15/61-71 Beauchamp Rd</li>
+                        <li>Matraville</li>
+                        <li>NSW </li>
+                        <li><a href="mailto:info@alchemyco.com.au?Subject=Please%20contact%20me">info@alchemyco.com.au</a></li>
+                    </ul>
+                </div>
+
+                <div className="rightColumn">
+                    <ul>
+                        <li>(02) 9550  1321</li>
+                        <li>Monday to Friyday:<br></br>8am - 4pm</li>
+                        <li>Saturday &amp; Sunday: Closed</li>
+                    </ul>
+                </div>
+            </div>
+
+            <form action="" method="" className="form">
+        
+                    <input className="input" type="email" placeholder="YOUR EMAIL ADDRESS"/>
+        
+                    <input className="input" type="text" placeholder="YOUR NAME"/>
+        
+                    <input className="input" type="text" placeholder="EMAIL SUBJECT"/>
+        
+                    <textarea className="input" id="coment" placeholder="YOUR MESSAGE"></textarea>
+        
+                    <input className="input" id="send" type="submit" value="SEND"/>
+            </form>
+        
+        </main>
+        
+    );
 }
-export default Contact;
+
+export default ContactPage;
