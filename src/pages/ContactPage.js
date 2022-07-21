f03aa5acef7270e04922e069810978828e2e3f82
DiegoRCA <diegocartelle@gmail.com>
2022-07-21 10:50:41 +1000

    contact add info

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -36,44 +36,52 @@ const ContactPage = (props)=>{
     }
 
     return(
-        <main className="holder">
-            <div className="d-flex w-100" >
-                <form action="/contacto" method="post" className="form d-flex w-100" onSubmit={handleSubmit} >
-                        <input className="input" type="email" placeholder="YOUR EMAIL ADDRESS"/>
-            
-                        <input className="input" type="text" placeholder="YOUR NAME"/>
-            
-                        <input className="input" type="text" placeholder="EMAIL SUBJECT"/>
-            
-                        <textarea className="input" id="coment" placeholder="YOUR MESSAGE"></textarea>
-                    <div className='d-flex w-100'>
-                        <div className='d-flex justify-content-center w-100 '>
-                        <input className="input" id="send" type="submit" value="SEND"/>
+
+            <main className="holder">
+                <div className="d-flex w-100" >
+                    <form action="/contacto" method="post" className="form" onSubmit={handleSubmit}>
+                    
+                        <input className="input" type="text" name="email" placeholder="YOUR EMAIL ADDRESS" 
+                        value={formData.email} onChange={handleChange}/>
+
+                        <input className="input" type="text" name="name" value={formData.nombre} onChange={handleChange} 
+                        placeholder="YOUR NAME"/>
+
+                        <input className="input" type="text" name="telephone" value={formData.telefono} onChange={handleChange} 
+                        placeholder="CELULAR"/>
+
+                        <textarea className="input" id="coment" name="mesage" value={formData.mensaje} onChange={handleChange} 
+                        placeholder="YOUR MESSAGE"/>
+
+                        {sending ? <p>Sending...</p> : null}
+                        {msg ? <p>{msg}</p> : null}
+                        <div className='d-flex w-100'>
+                            <div className='d-flex justify-content-center w-100 '>
+                                <input className="input" id="send" type="submit" value="SEND"/>
+                            </div>
                         </div>
-                    </div>
-                </form>
-            </div>
-            <div className="columns  d-block w-100">
-                <div className="rightColumn d-block  w-100">
-                    <ul>
-                            <li>(02) 9550  1321</li>
-                            <li>Monday to Friyday:<br></br>8am - 4pm</li>
-                            <li>Saturday &amp; Sunday: Closed</li>
-                    </ul>
-
-                    <div className="rightColumn d-block  w-100">
-
-                        <ul>
-                            <li>13-15/61-71 Beauchamp Rd</li>
-                            <li>Matraville</li>
-                            <li>NSW </li>
-                            <li><a className="mail"href="mailto:info@alchemyco.com.au?Subject=Please%20contact%20me">info@alchemyco.com.au</a></li>
-                        </ul>
-                    </div>
+                    </form>
                 </div>
-            </div>
-        
-        </main>
+                <div className="columns  d-block w-100">
+                            <div className="rightColumn d-block  w-100">
+                                <ul>
+                                        <li>(02) 9550  1321</li>
+                                        <li>Monday to Friyday:<br></br>8am - 4pm</li>
+                                        <li>Saturday &amp; Sunday: Closed</li>
+                                </ul>
+
+                                <div className="rightColumn d-block  w-100">
+
+                                    <ul>
+                                        <li>13-15/61-71 Beauchamp Rd</li>
+                                        <li>Matraville</li>
+                                        <li>NSW </li>
+                                        <li><a className="mail"href="mailto:info@alchemyco.com.au?Subject=Please%20contact%20me">info@alchemyco.com.au</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                </div>
+            </main>
         
     );
 }
